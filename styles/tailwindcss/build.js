39d58f324e220ca75be0ef7a0f2b57fbac221e4f
prosazhin <prosazhin@yandex.ru>
2023-08-01 23:47:49 +0200

    chore: update project

--- styles/tailwindcss/build.js
+++ styles/tailwindcss/build.js
@@ -7,29 +7,45 @@ const getTokensObject = (data) => {
     if (data[cur].hasOwnProperty('value')) {
       return { ...acc, [kebabCase(cur)]: data[cur].value };
     }
-    return { ...acc,
-      [kebabCase(cur)]: Object.fromEntries(Object.values(data[cur]).map((item) => [
-        kebabCase(item.path[item.path.length - 1]), item.value
-      ]))
+    return {
+      ...acc,
+      [kebabCase(cur)]: Object.fromEntries(
+        Object.values(data[cur]).map((item) => [
+          kebabCase(item.path[item.path.length - 1]),
+          item.value,
+        ]),
+      ),
     };
-  }, {})
+  }, {});
 };
 
-fs.writeFileSync('./styles/tailwindcss/index.js', `module.exports = {
+fs.writeFileSync(
+  './styles/tailwindcss/index.js',
+  `module.exports = {
   screens: ${JSON.stringify(getTokensObject(variables.screen))},
   colors: ${JSON.stringify(getTokensObject(variables.color))},
-  fontSize: ${JSON.stringify(['h', 't', 'tm'].reduce((acc, cur) => {
-    return { ...acc, ...Object.fromEntries(
-      Object.keys(variables.font[cur]).map((item) => [
-        `${cur}${item}`, [variables.font[cur][item].size.value, {
-          lineHeight: variables.font[cur][item].height.value,
-          fontWeight: variables.font[cur][item].weight.value,
-        }]
-      ])
-    )};
-  }, {}))},
+  fontSize: ${JSON.stringify(
+    ['h', 't', 'tm'].reduce((acc, cur) => {
+      return {
+        ...acc,
+        ...Object.fromEntries(
+          Object.keys(variables.font[cur]).map((item) => [
+            `${cur}${item}`,
+            [
+              variables.font[cur][item].size.value,
+              {
+                lineHeight: variables.font[cur][item].height.value,
+                fontWeight: variables.font[cur][item].weight.value,
+              },
+            ],
+          ]),
+        ),
+      };
+    }, {}),
+  )},
   borderRadius: ${JSON.stringify(getTokensObject(variables.rounded))},
   extend: {
     boxShadow: ${JSON.stringify(getTokensObject(variables.shadow))},
   }
-}`);
+}`,
+);
